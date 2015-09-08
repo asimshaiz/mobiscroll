ca6bea7fd3084cd076bf7c804a4c02348f32b5f1
asimshaiz <as@interactivemedica.com>
2015-09-08 15:12:11 +0200

    Create fi-FI.js

---
+++ js/i18n/fi-FI.js
@@ -0,0 +1,49 @@
+(function ($) {
+	$.mobiscroll.i18n['fi-FI'] = $.extend($.mobiscroll.i18n['fi-FI'], {
+		// Core
+		setText: 'joukko',
+		cancelText: 'peruuttaa',
+		clearText: 'kirkas',
+		selectedText: 'valinnut',
+		// Datetime component
+		dateFormat: 'd.M.yyyy',
+		dateOrder: 'dMyyyy',
+		dayNames: ['sunnuntai', 'maanantai', 'tiistai', 'keskiviikko', 'torstai', 'perjantai', 'lauantai'],
+		dayNamesShort: ['su', 'ma', 'ti', 'ke', 'to', 'pe', 'la'],
+		dayText: 'päivä',
+		hourText: 'tuntia',
+		minuteText: 'minuuttia',
+		monthNames: ['tammikuu', 'helmikuu', 'maaliskuu', 'huhtikuu', 'toukokuu', 'kesäkuu', 'heinäkuu', 'elokuu', 'syyskuu', 'lokakuu', 'marraskuu', 'joulukuu'],
+		monthNamesShort: ['tammi', 'helmi', 'maalis', 'huhti', 'touko', 'kesa', 'heina', 'elo', 'syys', 'loka', 'marras', 'joulu'],
+		monthText: 'kuukausi',
+		secText: 'sekuntia',
+		amText: 'AM',
+		pmText: 'PM',
+		timeFormat: 'HH.ii',
+		timeWheels: 'HHii',
+		yearText: 'vuosi',
+		nowText: 'nyt',
+		// Calendar component
+		firstDay: 0,
+		dateText: 'päivämäärä',
+		timeText: 'aika',
+		calendarText: 'kalenteri',
+		closeText: 'Sulje',
+		// Daterange component
+		fromText: 'Käynnistä',
+		toText: 'Loppu',
+		// Measurement components
+		wholeText: 'Koko',
+		fractionText: 'Murto',
+		unitText: 'Yksikkö',
+		// Time / Timespan component
+		labels: ['vuotta', 'kuukautta', 'paivaa', 'tuntia', 'minuuttia', 'sekuntia', ''],
+		labelsShort: ['Yrs', 'Mths', 'Days', 'Hrs', 'Mins', 'Secs', ''],
+		// Timer component
+		startText: 'Käynnistä',
+		stopText: 'Seis',
+		resetText: 'Palauta',
+		lapText: 'kierros',
+		hideText: 'Piilota'
+	});
+})(jQuery);
